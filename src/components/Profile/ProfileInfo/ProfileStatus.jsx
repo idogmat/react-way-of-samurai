ef0d2d241796469e1546f2e258f06fc37e357a43
Jack <40455730+idogmat@users.noreply.github.com>
2020-07-28 08:13:07 +0300

    +

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,34 +3,35 @@ import React from 'react'
 class ProfileStatus extends React.Component {
     state = {
         editMode: false,
-        title:'Yo'
+        title: 'Yo'
     }
 
-    activateEditMode=()=> {
+    activateEditMode = () => {
+        debugger;
+        console.log('this' + this);
         this.setState({
-            editMode:true
-        })
-        //setState-async method
-        // this.state.editMode=true;
-        //this.forceUpdate();
-        //forceUpdate-на крайний случай(форсит перерисовку)
-    }
-    deactivateEditMode=()=>{
-        this.setState({
-            editMode:false
+            editMode: true
         })
     }
+    deactivateEditMode = () => {
+        debugger;
+        console.log('this' + this);
+        this.setState({
+            editMode: false
+        })
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input type="text" onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                    <input type="text" onBlur={this.deactivateEditMode} value={this.props.status}/>
                 </div>
                 }
             </div>
